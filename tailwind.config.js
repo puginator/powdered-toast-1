f2ad896407f9e841b9d6b074098d1c6a0a0627ce
Austin Amento <puggsy31@aol.com>
2022-02-14 09:25:25 -0800

    working on ways to eat toast section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Astloch', 'cursive'],
+        secondary: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        poweredRed: "#844646",
+        darkGray: "#333333",
+        lightGray: "#777777",
+      },
+    },
+  },
+  plugins: [],
+};
