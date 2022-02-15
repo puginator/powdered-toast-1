853bc8a0bcfe55238e739fe03d27935dabb265ac
Austin Amento <puggsy31@aol.com>
2022-02-14 20:51:00 -0800

    added layoutchange to ways to eat section reponsiv

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const btn = document.querySelector('.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
 const menuClose = document.querySelector('.mobile-menu-close');
+const menuItems = document.querySelectorAll('.mobile-menu-item');
+const leftCarousel = document.querySelector('.carousel-left');
+const rightCarousel = document.querySelector('.carousel-right');
 
+
+
+
+// Nav Bar menu and button 
 
 btn.addEventListener('click', () => {
     console.log('clicked')
@@ -10,4 +17,24 @@ btn.addEventListener('click', () => {
 
 menuClose.addEventListener('click', () => {
     menu.classList.toggle('hidden');
+});
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    })
+});
+
+// Carousel Buttons and function
+function changeImage(a) {
+    document.getElementById('carousel-image').src = a;
+}
+
+leftCarousel.addEventListener('click', () => {
+    console.log('clicked')
+
+});
+rightCarousel.addEventListener('click', () => {
+    console.log('clicked')
+
 });
